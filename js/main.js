36651bddcc132aca27dce844b3f553580d63e2ed
mohamed gamal <mogamall086@gmail.com>
2023-08-21 03:25:04 +0300

    js code almost done

--- js/main.js
+++ js/main.js
@@ -1,108 +1,82 @@
-let mySearchValue = document.getElementById("searchValue")
+// prepare the day name
+function prepareDate() {
+  const date = new Date();
+  // Get the day of the week as a number (0 = Sunday, 6 = Saturday)
+  const dayOfWeekNumber = date.getDay();
 
-// send request depending on place
-function myRequest (place){
-    let myHttp = new XMLHttpRequest();
-    myHttp.open("GET", `https://api.weatherapi.com/v1/forecast.json?key=de7b5255b04e40bcb69234630221401&q=${place}&days=3`)
-    
-    myHttp.send()
+  // Array of days of the week
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    myHttp.addEventListener("readystatechange", function(){
-        if(myHttp.readyState == 4 && myHttp.status == 200){
-            let data = JSON.parse(myHttp.response)
-            console.log(data);  
-            displayData(data)
-        }
-    })
+  // Get the day of the week as a string
+  const dayOfWeekString = daysOfWeek[dayOfWeekNumber];
+
+  return [
+    daysOfWeek[dayOfWeekNumber], // today
+    daysOfWeek[dayOfWeekNumber + 1 == 7 ? 0 : dayOfWeekNumber + 1], // tomorrow
+    daysOfWeek[dayOfWeekNumber + 2 == 8 ? 1 : dayOfWeekNumber + 2], // after tomorrow
+  ];
 }
 
-// giv request function a value from search input using search function
-function search(){
-    if(mySearchValue.value !== "") myRequest(mySearchValue.value)
+// this function show cairo when page is loaded
+(async function () {
+  let data = await getCurrentDay();
+  display(data);
+})();
+
+// when i write inside input do this function
+document
+  .getElementById("searchIndput")
+  .addEventListener("keyup", async function () {
+    if (this.value.length >= 4) {
+      let data = await getCurrentDay(this.value);
+      console.log(data);
+      display(data);
+    }
+  });
+
+// get data depend on argument passed
+async function getCurrentDay(location = "cairo") {
+  let res = await fetch(
+    `https://api.weatherapi.com/v1/forecast.json?key=4acda80e3cba426f8e2212404232008&q=${location}&aqi=yes&days=3`
+  );
+  let data = await res.json();
+  return data;
 }
 
-// array of elements of city in html
-let myCityElement = Array.from(document.querySelectorAll(".city"))
-for(let i = 0; i < myCityElement.length;i++){
-    myCityElement[i].style.visibility = "hidden"
-}
+// show all data in page depending on data send
+function display(allData) {
+  let cartona = "";
+  for (let i = 0; i <= 2; i++) {
+    cartona += `<div class="col-lg-4 col-md-6 mb-4">
+    <div class="item mb-3">
+        <div class="head fw-bold text-capitalize d-flex justify-content-between align-items-center p-2">
+            <p class="m-0">${prepareDate()[i]}</p>
+            <p class="m-0">${allData.forecast.forecastday[i].date}</p>
+        </div>
+        <div class="body p-3">
+            <p class="mt-1 fw-bold">${allData.location.name}</p>
+            <p class="my-1 degree fw-bold">${allData.forecast.forecastday[i].day.avgtemp_c}<sup>o</sup>C</p>
+            <div class="icon my-1 ms-3 fs-2">
+            <img class="w-25" src="${allData.forecast.forecastday[i].day.condition.icon}"/>
+            </div>
+            <p class="my-2 clear fw-bold">${allData.forecast.forecastday[i].day.condition.text}</p>
+        </div>
+        <div class="footer d-flex p-2">
+            <div class="cont me-3"><i class="fa-solid fa-umbrella"></i> ${allData.forecast.forecastday[i].day.maxwind_kph}%</div>
+            <div class="cont me-3"><i class="fa-solid fa-wind"></i> ${allData.forecast.forecastday[i].day.maxwind_mph}km/h</div>
+            <div class="cont me-3"><i class="fa-regular fa-compass"></i> ${allData.forecast.forecastday[i].hour[0].wind_dir}</div>
+        </div>
+    </div>
+  </div>`;
+  }
 
-// array of element of day date in html
-let mySpanDay =Array.from(document.querySelectorAll(".day"));
-let myPartly =Array.from(document.querySelectorAll(".partly"));
-let secondTemp =document.querySelector(".secondTemp")
-let firstImg =document.querySelector(".firstImg")
-let secondImg =document.querySelector(".secondImg")
-let threetemp =document.querySelector(".threetemp")
-let threeImg =document.querySelector(".threeImg")
-let firstDgree =document.querySelector(".firstDgree")
-
-
-function displayData(data){
-
-    // start name city
-    let city = data.location.name
-    for(let i = 0; i < myCityElement.length; i++){
-        if(i == 0){
-            myCityElement[i].innerHTML = city
-            myCityElement[i].style.visibility = "visible"
-        }
-        else{
-            myCityElement[i].style.visibility = "hidden"
-        }
-    }
-    // end name city
-
-    // start date 
-    let arrayOfDays = data.forecast.forecastday
-    for(let i = 0; i < mySpanDay.length; i++){
-        mySpanDay[i].innerHTML = arrayOfDays[i].date
-    }
-    // end date
-
-    // start weather type
-   let myWeatherType = data.current.condition.text
-    for(let i = 0; i < myPartly.length; i++){
-        if(i == 0){
-            myPartly[i].innerHTML = myWeatherType;
-            myPartly[i].style.visibility = "visible"; 
-        }else{
-            myPartly[i].style.visibility = "hidden"
-        }
-    }
-    // end weather type
-
-    // start second temp
-    secondTemp.innerHTML = data.current.temp_c 
-    secondTemp.innerHTML += "<sup>o</sup>"
-    secondTemp.innerHTML += "c"
-    // end second temp
-
-    // get first icon
-    console.log(data.current.condition.icon);
-    firstImg.src = data.current.condition.icon
-    // end first icon
-
-    // get second icon
-    let secondIcon = data.forecast.forecastday[1].day.condition.icon
-    secondImg.src = secondIcon
-    // end second icon
-
-    // get thired temp
-    firstDgree.innerHTML = data.forecast.forecastday[0].day.avgtemp_c 
-    firstDgree.innerHTML += "<sup>o</sup>"
-    firstDgree.innerHTML += "c"
-    // end thired temp
-
-    // get thired icon
-        threeImg.src = data.forecast.forecastday[2].day.condition.icon
-    // end thired icon
-
-
-    // get first dgree
-    secondTemp.innerHTML = data.current.temp_c 
-    secondTemp.innerHTML += "<sup>o</sup>"
-    secondTemp.innerHTML += "c"
-    // end first dgree
-
+  document.getElementById("myRow").innerHTML = cartona;
 }
